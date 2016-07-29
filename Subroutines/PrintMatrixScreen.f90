630551cc9a9ae1f46342e9e7dc689da692c9c7b7
Jonathan Kung <shuruken@gmail.com>
2016-07-29 15:07:17 -0600

    Update PrintMatrixScreen.f90

--- Subroutines/PrintMatrixScreen.f90
+++ Subroutines/PrintMatrixScreen.f90
@@ -4,6 +4,6 @@ subroutine printmatrixscreen(array,mrow,mcol)
       integer, intent(in) :: mrow,mcol
       integer :: k
       do k=1,mrow
-         write(6,*) array(k,:)
+         write(6,*) array(:,k)
       enddo
 endsubroutine printmatrixscreen
